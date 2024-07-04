9efd1fa91d8cc2f9df92e8194c075a09c71f94ac
Ashdevelopernewbie <aahashish100@gmail.com>
2024-07-04 14:20:09 +0545

    refactor: removed unwanted routes

--- cmd/main.go
+++ cmd/main.go
@@ -12,25 +12,15 @@ func main() {
 
 	router.Static("/public", "./public")
 
-	router.GET("/admin", func(ctx *gin.Context) {
-		controller.AdminGETController(ctx)
-	})
-	router.POST("/admin", func(ctx *gin.Context) {
-		controller.AdminPOSTController(ctx)
-	})
-	router.GET("/admin/dashboard", func(ctx *gin.Context) {
-		controller.AdminDashboardController(ctx)
-	})
-
-	v2 := router.Group("/")
+	v1 := router.Group("/")
 	{
-		v2.GET("/", func(ctx *gin.Context) {
+		v1.GET("/", func(ctx *gin.Context) {
 			controller.HomeController(ctx)
 		})
-		v2.GET("/bubble", func(ctx *gin.Context) {
+		v1.GET("/bubble", func(ctx *gin.Context) {
 			controller.GetBubbleController(ctx)
 		})
-		v2.POST("/bubble", func(ctx *gin.Context) {
+		v1.POST("/bubble", func(ctx *gin.Context) {
 			controller.PostBubbleController(ctx)
 		})
 	}
